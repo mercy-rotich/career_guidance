6afcc46ede831e9577129665ab6f5c52fcc86b8e
Mercy Cherotich <cherotichm182@gmail.com>
2025-03-29 21:26:06 +0300

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import Login from './Components/LoginPage/Login';
 import { Route, Routes, Navigate } from 'react-router-dom';
 import SignUp from './Components/SignupPage/SignUp';
+import Navbar from './Components/Common/Navbar/Navbar';
 
 const App = () => {
   return (
     <div>
+      <Navbar/>
       <Routes>
         <Route path="/" element={<Navigate to="/login" replace />} />
         <Route path="/login" element={<Login />} />
---
+++ src/Components/Common/Navbar/Navbar.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+import './Navbar.css'
+
+const Navbar = () => {
+  const [menu,setMenu]= useState("")
+  return (
+    <div className='navbar'>
+      <div className="logo">
+      <Link to="/">PathFinder</Link>
+        </div>
+      <nav>
+<ul className="navbar-menu">
+  <li><Link to="/dashboard" onClick={()=>setMenu("dashboard")} className={menu==="dashboard"?"active":""}>Dashboard</Link></li>
+  <li><Link to="/assesments" onClick={()=>setMenu("assesments")} className={menu==="assesments"?"active":""}>Assesments</Link></li>
+  <li><Link to="/career" onClick={()=>setMenu("career")} className={menu==="career"?"active":""}>Career Explorer</Link></li>
+  <li><Link to="/learning" onClick={()=>setMenu("learning")} className={menu==="learning"?"active":""}>Learning</Link></li>
+  <li><Link to="/network" onClick={()=>setMenu("network")} className={menu==="network"?"active":""}>Network</Link></li>
+</ul>
+      </nav>
+      <div className="login-btn">
+        <Link to="/signup"><button>Login</button></Link>
+      </div>
+      <div className="user-profile">
+        <span>Alex Johnson</span>
+        <div className="user-avatar">AJ</div>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
